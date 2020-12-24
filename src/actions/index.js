93f4cd20c00cc4acf808839f463d1ba2f42689b7
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-24 09:52:19 +0700

    change passed data to addIncome action to object

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes'
 
 export const saveUser = (id, username) => ({ type: types.SAVE_USER, id, username })
 export const removeUser = () => ({ type: types.REMOVE_USER })
-export const addIncome = (id, amount, desc) => ({ type: types.ADD_INCOME, id, amount, desc})
+export const addIncome = (data) => ({ type: types.ADD_INCOME, data})
 export const addExpense = (id, amount, desc) => ({ type: types.ADD_EXPENSE, id, amount, desc})
 export const deleteIncome = (id) => ({ type: types.DELETE_INCOME, id })
 export const deleteExpense = (id) => ({ type: types.DELETE_EXPENSE, id })
