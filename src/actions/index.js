55ceac98182468bd2f4f521f678605feb7c304fa
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-24 14:59:37 +0700

    change addexpense actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import * as types from '../constants/ActionTypes'
 export const saveUser = (id, username) => ({ type: types.SAVE_USER, id, username })
 export const removeUser = () => ({ type: types.REMOVE_USER })
 export const addIncome = (data) => ({ type: types.ADD_INCOME, data})
-export const addExpense = (id, amount, desc) => ({ type: types.ADD_EXPENSE, id, amount, desc})
+export const addExpense = (data) => ({ type: types.ADD_EXPENSE, data})
 export const deleteIncome = (id) => ({ type: types.DELETE_INCOME, id })
 export const deleteExpense = (id) => ({ type: types.DELETE_EXPENSE, id })
 export const setLiffData = (data) => ({ type: types.SET_LIFF_DATA, data })
