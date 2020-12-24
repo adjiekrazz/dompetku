9ca2c5561cf7be4ee9bda6a7d92d8e0b13b6028d
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-24 14:38:26 +0700

    add expense modified from income

--- src/pages/Expense.js
+++ src/pages/Expense.js
@@ -1,18 +1,120 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Row, Col, Card, Table, Button } from 'react-bootstrap'
+import { Row, Col, Card, Table, Button, Modal, Form } from 'react-bootstrap'
+import { rupiah, shortDate } from '../utils'
+
+class AddModal extends React.Component {
+  constructor(props) {
+    super(props);
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    this.state = {
+      date: today,
+      amount: 0,
+      desc: ''
+    }
+  }
+
+  handleChange(event) {
+    this.setState({ ...this.state, date: event.target.value })
+  }
+
+  handleSubmit = () => {
+    this.props.onSubmit(this.state)
+  }
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Tambah Pengeluaran
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicDate">
+              <Form.Label>Tanggal</Form.Label>
+              <Form.Control name="date" type="date" onChange={() => this.setState({ ...this.state, date: event.target.value })} />
+              <Form.Text className="text-muted">
+                Jika kosong, otomatis memakai tanggal sekarang.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicAmount">
+              <Form.Label>Jumlah</Form.Label>
+              <Form.Control name="amount" type="number" onChange={() => this.setState({ ...this.state, amount: event.target.value })} />
+              <Form.Text className="text-muted">
+                Masukkan hanya angka, tanpa memakai titik ataupun koma.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicDesc">
+              <Form.Label>Keterangan</Form.Label>
+              <Form.Control name="desc" as="textarea" rows={2} onChange={() => this.setState({ ...this.state, desc: event.target.value })} />
+              <Form.Text className="text-muted">
+                Tambahkan keterangan, misalnya <i>Cicilan Motor</i>.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group>
+              
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className="text-center">
+          <Button onClick={() => this.handleSubmit()} className="mr-2">Tambah</Button>
+          <Button onClick={this.props.onHide} variant="danger">Batal</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
 
 class Expense extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      addModalShow: false,
+      total: 'Rp. 0,00'
+    }
   }
+
+  componentDidMount() {
+    if (this.props.expense.length !== 0) {
+      this.setState({ total: rupiah(this.props.expense.reduce((first, n) => Number(first.amount) + Number(n.amount)), 0, true) })
+    }
+  }
+
+  getID() {
+    var id;
+    if (this.props.expense.length === 0) {
+      id = 1;
+    } else {
+      id = this.props.expense[this.props.expense.length - 1].id + 1;
+    }
+    return id;
+  }
+
+  handleSubmit(data) {
+    this.props.addIncome({ id: this.getID(), ...data})
+    this.setState({ addModalShow: false })
+  }
+
   render() {
     return (
       <Row>
         <Col className="col-lg-12 mb-4">
           <Card>
             <Card.Body>
-              <Card.Text>Total Pengeluaran : Rp. 18.000,00</Card.Text>
+              <Card.Text>Total Pengeluaran : {this.state.total}</Card.Text>
               <Table striped bordered hover size="sm">
                 <thead>
                     <tr>
@@ -25,17 +127,22 @@ class Expense extends React.Component {
                   {this.props.expense.length > 0 ? this.props.expense.map((value, key) => {
                     return (
                       <tr key={key}>
-                        <td>{value.date}</td>
-                        <td>{value.amount}</td>
+                        <td>{shortDate(value.date)}</td>
+                        <td>{rupiah(value.amount)}</td>
                         <td>{value.desc}</td>
                       </tr>
                     )
                   }) : (<tr><td className="text-center" colSpan="3">Belum ada pengeluaran.</td></tr>) }
                 </tbody>
               </Table>
-              <Button variant="success" size="sm" block>Tambah Pengeluaran</Button>
+              <Button variant="success" size="sm" block onClick={() => this.setState({ addModalShow: true})}>Tambah Pengeluaran</Button>
             </Card.Body>
           </Card>
+          <AddModal
+            show={this.state.addModalShow}
+            onHide={() => this.setState({ addModalShow: false })}
+            onSubmit={this.handleSubmit.bind(this)}
+          ></AddModal>
         </Col>
       </Row>
     )
@@ -44,13 +151,15 @@ class Expense extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    expense: state.wallet.expense
+    expense: state.expense
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    addExpense: (data) => { 
+      dispatch({ type: 'ADD_EXPENSE', data })
+    }
   }
 }
 
--- src/reducers/expense.js
+++ src/reducers/expense.js
@@ -1,15 +1,17 @@
 import { ADD_EXPENSE, DELETE_EXPENSE } from '../constants/ActionTypes'
 
-const initialState = localStorage.expense_data || [{}]
+const initialState = localStorage.expense_data ? JSON.parse(localStorage.getItem('expense_data')) : [];
 
 export default function expense(state = initialState, action) {
     switch (action.type) {
         case ADD_EXPENSE:
-            let newExpense = state.expense.slice()
-            newExpense.splice(action.id, action.date, 0, action.amount, action.desc)
-            return newState
+            localStorage.setItem('expense_data', JSON.stringify([...state, action.data]))
+            return [
+                    ...state,
+                    action.data
+                ]
         case DELETE_EXPENSE:
-            return state.expense.filter((id) => id !== action.id)
+            return state.filter((id) => id !== action.id);
         default: 
             return state;
     }
