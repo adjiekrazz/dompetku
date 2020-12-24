527d9f08463052e1285bec9e173b2b820b770f78
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-24 16:24:05 +0700

    add add some menu for login nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import { connect } from 'react-redux'
 
@@ -11,25 +12,24 @@ class Navigation extends React.Component {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Dompetku</Navbar.Brand>
+                <Link to="/" className="navbar-brand" variant="dark">My Wallet</Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Link to="/income" className="nav-link">Income</Link>
+                        <Link to="/expense" className="nav-link">Expense</Link>
+                        <NavDropdown title="Report" id="collapsible-nav-dropdown">
+                            <Link to="/savepdf" className="dropdown-item">Save as PDF</Link>
+                            <Link to="/sendemail" className="dropdown-item">Send to Email</Link>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <Link to="/share" className="dropdown-item">Share to ..</Link>
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#deets">Pengaturan</Nav.Link>
-                    <Nav.Link href="#" onClick={this.props.handleLogout()}>
+                    <Link to="/about" className="nav-link">About</Link>
+                    <Link to="#" className="nav-link" onClick={this.props.handleLogout()}>
                         Keluar
-                    </Nav.Link>
+                    </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
