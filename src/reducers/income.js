d3c7146e7c7a62cdc4ee87aa08d27d0cf7bd98df
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-24 13:13:26 +0700

    change income reducer to save in local storage

--- src/reducers/income.js
+++ src/reducers/income.js
@@ -1,16 +1,17 @@
 import { ADD_INCOME, DELETE_INCOME } from '../constants/ActionTypes'
 
-const initialState = localStorage.income_data || []
+const initialState = localStorage.income_data ? JSON.parse(localStorage.getItem('income_data')) : [];
 
 export default function income(state = initialState, action) {
     switch (action.type) {
         case ADD_INCOME:
+            localStorage.setItem('income_data', JSON.stringify([...state, action.data]))
             return [
                     ...state,
-                    { ...action.data }
+                    action.data
                 ]
         case DELETE_INCOME:
-            return state.income.filter((id) => id !== action.id);
+            return state.filter((id) => id !== action.id);
         default: 
             return state;
     }
